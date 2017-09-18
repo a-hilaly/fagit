09efec240aabd6534b2412544491b5b59af9c337
A.H <hilalyamine@gmail.com>
2017-09-18 20:57:34 +0200

    Rnn

--- run.sh
+++ run.sh
@@ -2,10 +2,13 @@
 
 
 _GIT_USER="ial-bot"
-_GIT_PW="uehMLMRwIALBOT0"
+_GIT_PW=""
 _ORGANISATION="iallabs"
 export _GIT_USER
 export _GIT_PW
 export _ORGANISATION
 
+#
+#
+#
 python3 bavtu-sos-packaging.py "$@"
