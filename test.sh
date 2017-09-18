c46da7159c596cce401972bfec6d9d49a45a9121
A.H <hilalyamine@gmail.com>
2017-09-18 23:04:08 +0200

    Run

--- test.sh
+++ test.sh
@@ -7,8 +7,16 @@ function __test_build () {
     bash run.sh --build __test__ -p
 }
 
-function __assert_tests () {
-    __test_clone
+function __test_make () {
+    bash run.sh --make $PWD
+}
+
+function __clear_tests () {
+    dir="$HOME/__test__"
+    rm -rf $dir
+}
+
+function __assert_tests_1 () {
     dir="$HOME/__test__"
     if [ -d "$dir" ]; then
         echo "[ OK ] ... test_clone successfull"
@@ -17,11 +25,9 @@ function __assert_tests () {
         echo "Exiting tests ..."
         exit 1
     fi
-    __test_build
     file="$dir/output"
     if [ -f "$file" ]; then
         echo "[ OK ] ... test_build successfull"
-        return 0
     else
         echo "[WARN] ... test_build failed"
         exit 1
@@ -29,10 +35,33 @@ function __assert_tests () {
     echo "end tests"
 }
 
-function __clear_tests () {
-    dir="$HOME/__test__"
-    rm -rf $dir
+function __asert_tests_2 () {
+  dir="$HOME/__test__"
+  if [ -d "$dir" ]; then
+      echo "[ OK ] ... test_make part clone successfull"
+  else
+      echo "[WARN] ... test_make part clone failed"
+      echo "Exiting tests ..."
+      exit 1
+  fi
+  file="$dir/output"
+  if [ -f "$file" ]; then
+      echo "[ OK ] ... test_make part build  successfull"
+  else
+      echo "[WARN] ... test_make part build failed"
+      exit 1
+  fi
+  echo "end tests"
 }
 
-__assert_tests
-__clear_tests
+function run_tests () {
+    __test_clone
+    __test_build
+    __assert_tests_1
+    __clear_tests
+    __test_make
+    __assert_tests_2
+    __clear_tests
+}
+
+run_tests
